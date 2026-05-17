ea74fa0ea8bb251ed3c060b12b05f7cb19064dd2
Vijay-361 <rr3079244@gmail.com>
2026-05-17 14:35:58 +0530

    added authControllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,49 @@
+const bcrypt = require("bcrypt.js");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (userId) =>
+    jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn : "7d"});
+
+const register = async(req,res) =>
+{
+    try
+    {
+        const{name, email, password} = req.body;
+        if(!name || !email || !password)
+        {
+            return res.status(400).json({message : "All fields are required"});
+        }
+
+        const existing = await User.findOne({ email: email.toLowerCase() });
+        if (existing)
+        return res.status(400).json({ message: "Email already registered" });
+    
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User(
+            {
+                name,
+                email,
+                password : hashedpassword
+            }
+        );
+        await user.save();
+        const token = generateToken(user._id);
+         res.status(201).json({
+      message: "Account created successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        balance: user.balance,
+      },
+    });
+}
+catch(error)
+{
+    res.status(500).json({message : "Server error during registration"});
+}
+};
