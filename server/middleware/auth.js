f79479233f48e62cd47f2c09b75fd36a0c0ebf46
Vijay-361 <rr3079244@gmail.com>
2026-05-17 14:16:54 +0530

    Added middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async(req, res, next) =>
+{
+    try
+    {
+        const authHeader = req.headers.auth;orization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer"))
+        {
+            return res.status(401).json({message :"No token,access denied"});
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user)
+        {
+           return res.status(401).json({message : "User not found"});
+        }
+        req.user = user;
+        next();
+
+    }
+    catch(error)
+    {
+        return res.status(401).json({message : "Token invlid or expired"});
+    }
+};
+module.exports = protect;
